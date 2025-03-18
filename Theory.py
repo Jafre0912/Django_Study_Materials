79fbbad7445db7508024b6ff4447b487e7756d72
JAFRE_ALAM <112960806+Jafre0912@users.noreply.github.com>
2025-03-18 21:59:18 +0000

    DjangoTheoryPart

---
+++ Theory.py
@@ -0,0 +1,6 @@
+# Django Python Framework : it's an open source django framework work on MVT model(Model View Template)
+# MODEL (used for database): where we managed all the working related database
+# view (used for backend): Django views ka kaam hota hai request ko process karna aur response dena—chahe wo HTML page ho, JSON API response ho, ya kuch aur.
+# Template (used for frontend) : Django templates frontend ka kaam karte hain
+
+# installing djnago App: django-admin startproject MyApp
