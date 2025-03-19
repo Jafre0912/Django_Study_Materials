ddb01555a3ed2ba0718965089808bb1fe9829abd
JAFRE_ALAM <112960806+Jafre0912@users.noreply.github.com>
2025-03-19 00:30:32 +0000

    theory

---
+++ Theory/MethoTheory.py
@@ -0,0 +1,11 @@
+# GET vs POST: Key Differences 🚀
+#Feature                         	GET	                                                POST
+#Purpose	                  Data fetch karne ke liye	                           Data send karne ke liye
+#Data                Visibility	URL me dikhta hai (Query Params)	           Body me hota hai, hidden
+#Security	           Kam secure (Data URL me dikh jata hai)	               Zyada secure (Data hidden rehta hai)
+#Data Length	         Limited (URL length (1024 char)limit hoti hai)	       Unlimited (Body me bheja jata hai)
+#Use Case	        Search, user profile dekhna	Login, sign-up,                form submission
+
+#PUT method: data ko update karne k lye
+#DELETRE Method: data ko delete krne k lye
+#PATCH method: partially update krne k lye
